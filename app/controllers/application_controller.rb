48dd232877d698917250815f54ed4a8b4bec9c02
manoel bertin <manoeltirj@gmail.com>
2023-03-31 21:07:36 -0300

    foi feito um before action para acrescentar o NOME no

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
-module Storefront::V1
-  class ApiController < ApplicationController
-    include Authenticatable
+class ApplicationController < ActionController::API
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
+  protected
+  
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password, :password_confirmation])
   end
 end
