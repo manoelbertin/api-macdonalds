a4b125edabd99140da420977e9f67c1160a4cf05
manoel bertin <manoeltirj@gmail.com>
2023-04-06 17:31:41 -0300

    Configuring tests with RSpec started working again

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,10 +1,6 @@
 require 'spec_helper'
-require 'rspec/rails'
-
-Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
-# Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 
