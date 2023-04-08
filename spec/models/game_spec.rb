6579d3329a0797eb5fe5d1ef41eb1b33c7f53650
manoel bertin <manoeltirj@gmail.com>
2023-04-08 13:55:31 -0300

    Add the validation testes of the model Game.

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Game, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to validate_presence_of(:mode) }
+  it { is_expected.to validate_presence_of(:release_date) }
+  it { is_expected.to validate_presence_of(:developer) }
+
+  it { is_expected.to belong_to :system_requirement }
 end
