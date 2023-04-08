640918256f6be86195702b27aef42df0d8c490d5
manoel bertin <manoeltirj@gmail.com>
2023-04-08 13:59:29 -0300

    Verify if the field mode is an enum (1- people vs p,2- p vs enviroment, or 3- both)

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Game, type: :model do
   it { is_expected.to validate_presence_of(:mode) }
+  it { is_expected.to define_enum_for(:mode).with_values({ pvp: 1, pve: 2, both: 3 }) }
+
   it { is_expected.to validate_presence_of(:release_date) }
   it { is_expected.to validate_presence_of(:developer) }
 
