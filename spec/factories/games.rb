d286d38070582b29d8a3d3241fc56a2cf4e1a069
manoel bertin <manoeltirj@gmail.com>
2023-04-08 16:39:10 -0300

    the system_requirement was associated with the FactoryBot game.

--- spec/factories/games.rb
+++ spec/factories/games.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :game do
     mode { %i(pvp pve both).sample }
     release_date { "2023-04-08 13:51:24" }
-    developer { "MyString" }
-    system_requirement { nil }
+    developer { Faker::Company.name }
+    system_requirement
   end
 end
