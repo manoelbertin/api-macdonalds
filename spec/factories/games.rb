452e7149d1a1fea3046abbe8a5ad39d62cc6af92
manoel bertin <manoeltirj@gmail.com>
2023-04-08 16:37:17 -0300

    Altered the factory of game.

--- spec/factories/games.rb
+++ spec/factories/games.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :game do
-    mode { 1 }
+    mode { %i(pvp pve both).sample }
     release_date { "2023-04-08 13:51:24" }
     developer { "MyString" }
     system_requirement { nil }
