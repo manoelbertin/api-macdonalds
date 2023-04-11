ebb915e39a2be697bb08456033e93f4b6ad43f3e
manoel bertin <manoeltirj@gmail.com>
2023-04-11 11:36:47 -0300

    the code was commented here.

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -3,4 +3,9 @@ require 'rails_helper'
 RSpec.describe Category, type: :model do
   it { is_expected.to validate_presence_of(:name) }
   it { is_expected.to validate_uniqueness_of(:name).case_insensitive }
-end
+
+  it { is_expected.to have_many(:product_categories).dependent(:destroy) }
+  # se eu apagar uma categoria, todas associações com essa categoria apagada será destruida
+  it { is_expected.to have_many(:products).through(:product_categories) }
+  # através dessa relação q categoria tem com product_category, vamos ter uma
+  # relação DIRETA com nosso produto.
