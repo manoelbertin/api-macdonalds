20c09829b4df2fc9dcd20f6d95873b9bd3f916f3
manoel bertin <manoeltirj@gmail.com>
2023-04-12 11:10:56 -0300

    we need to comtemplate three scenarios to the tests of the date.

--- spec/validators/future_date_validator_spec.rb
+++ spec/validators/future_date_validator_spec.rb
@@ -8,4 +8,16 @@ end
 
 describe FutureDateValidator do
   subject { Validatable.new }
+
+  context "when date is before current date" do
+    before { subject.date = 1.day.ago }
+  
+    it "should be invalid" do
+      expect(subject.valid?).to be_falsey
+    end
+    
+    it "adds an error on model" do
+      subject.valid?
+      expect(subject.errors.keys).to include(:date)
+    end
 end
