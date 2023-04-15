0a29ea0c88e3067988ad8a343ea3be273ea493aa
manoel bertin <manoeltirj@gmail.com>
2023-04-15 12:25:38 -0300

    o método auth_header p receber um usuário específico e retornar o cabeçalho.

--- spec/support/request_api.rb
+++ spec/support/request_api.rb
@@ -5,4 +5,11 @@ module RequestAPI
   rescue
     return {} 
   end
+
+  def auth_header(user = nil, merge_with: {})
+    user ||= create(:user)
+    auth = user.create_new_auth_token
+    header = auth.merge({ 'Content-Type' => 'application/json', 'Accept' => 'application/json' })
+    header.merge merge_with
+  end
 end
