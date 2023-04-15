35f3cc7b73caba738a4e2a0dad64ef713552b108
manoel bertin <manoeltirj@gmail.com>
2023-04-15 12:40:15 -0300

    Estamos enviando uma requisição GET para /admin/v1/home, utilizando o método auth_user com o usuário que criamos no let e utilizando médoto body_json p retornar o body formatado da resposta e validando se é um que esperamos.

--- spec/requests/admin/v1/home_spec.rb
+++ spec/requests/admin/v1/home_spec.rb
@@ -7,4 +7,9 @@ describe "Home", type: :request do
     get '/admin/v1/home', headers: auth_header(user)
     expect(body_json).to eq({ 'message' => 'Uhul!' })
   end
+
+  it "tests home" do
+    get '/admin/v1/home', headers: auth_header(user)
+    expect(response).to have_http_status(:ok)
+  end
 end
