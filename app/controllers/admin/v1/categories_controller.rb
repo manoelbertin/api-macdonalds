2fa7fc553690275cf0acc43554f6e50f26424f29
manoel bertin <manoeltirj@gmail.com>
2023-04-25 08:17:59 -0300

    para melhorar um pouco mais o controller, vamos criar um método para salvar a categoria e limpar a 'action create'.

--- app/controllers/admin/v1/categories_controller.rb
+++ app/controllers/admin/v1/categories_controller.rb
@@ -20,5 +20,12 @@ module Admin::V1
       return {} unless params.has_key?(:category)
       params.require(:category).permit(:id, :name)
     end
+
+    def save_category!
+      @category.save!
+      render :show
+    rescue
+      render_error(fields: @category.errors.messages)
+    end
   end
 end
