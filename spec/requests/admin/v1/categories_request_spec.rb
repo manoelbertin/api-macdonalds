e47f07484f8dd457bd110f1f88a14c654081441c
manoel bertin <manoeltirj@gmail.com>
2023-04-27 08:59:10 -0300

    Por último temos que validar que as ProductCategory que não são associadas à category, precisamos dessa vez criarmos o ProductCategory com o create}_list que não tenham category associado e chamamos a rota. Em seguida, extraímos os IDs dos ProductCategory que criamos, carreegamos do model e verificamos se eles continuam presentes.

--- spec/requests/admin/v1/categories_request_spec.rb
+++ spec/requests/admin/v1/categories_request_spec.rb
@@ -142,5 +142,11 @@ RSpec.describe "Admin::V1::Categories", type: :request do
     expect(expected_product_categories.count).to eq 0
   end
 
-  
+  it 'does not remove unassociated product categories' do
+    product_categories = create_list(:product_category, 3)
+    delete url, headers: auth_header(user)
+    present_product_categories_ids = product_categories.map(&:id)
+    expected_product_categories = ProductCategory.where(id: present_product_categories_ids)
+    expect(expected_product_categories.ids).to contain_exactly(*present_product_categories_ids)
+  end
 end
