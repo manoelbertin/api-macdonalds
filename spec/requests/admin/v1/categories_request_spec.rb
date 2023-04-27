252c2779915ca69e2d8b7680a93c29654ac5d355
manoel bertin <manoeltirj@gmail.com>
2023-04-27 08:25:50 -0300

    Agora verificamos se a response realmente está sem conteúdo no body.

--- spec/requests/admin/v1/categories_request_spec.rb
+++ spec/requests/admin/v1/categories_request_spec.rb
@@ -128,5 +128,10 @@ RSpec.describe "Admin::V1::Categories", type: :request do
     delete url, headers: auth_header(user)
     expect(response).to have_http_status(:no_content)
   end
+
+  it 'does not return any body content' do
+    delete url, headers: auth_header(user)
+    expect(body_json).to_not be_present
+  end
   
 end
