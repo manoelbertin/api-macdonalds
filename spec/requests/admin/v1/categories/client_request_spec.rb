0caa43bc105cd063082b4a445579c5e0f9384e9e
manoel bertin <manoeltirj@gmail.com>
2023-05-01 19:55:00 -0300

    Então para cada context, estamos chamando as rotas de cada um dentro de before(:each).

--- spec/requests/admin/v1/categories/client_request_spec.rb
+++ spec/requests/admin/v1/categories/client_request_spec.rb
@@ -6,18 +6,26 @@ RSpec.describe "Admin V1 Categories as :client", type: :request do
   context "GET /categories" do
     let(:url) { "/admin/v1/categories" }
     let!(:categories) { create_list(:category, 5) }
+
+    before(:each) { get url, headers: auth_header(user) }
   end
   
   context "POST /categories" do
     let(:url) { "/admin/v1/categories" }
+
+    before(:each) { post url, headers: auth_header(user) }
   end
   
   context "PATCH /categories/:id" do
     let(:category) { create(:category) }
     let(:url) { "/admin/v1/categories/#{category.id}" }
+
+    before(:each) { patch url, headers: auth_header(user) }
   end
   
   context "DELETE /categories/:id" do
     let!(:category) { create(:category) }
     let(:url) { "/admin/v1/categories/#{category.id}" } 
-  endend
+
+    before(:each) { delete url, headers: auth_header(user) }
+  end
