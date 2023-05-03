3778cbdc813c088e78f2f9ad81f781970e8c2d6a
manoel bertin <manoeltirj@gmail.com>
2023-05-03 08:29:41 -0300

    Com os testes feitos para o perfil client, onde esperamos o acesso proibido, podemos fazer o mesmo para o acesso não autenticado.

--- spec/requests/admin/v1/categories/client_request_spec.rb
+++ spec/requests/admin/v1/categories/client_request_spec.rb
@@ -8,12 +8,14 @@ RSpec.describe "Admin V1 Categories as :client", type: :request do
     let!(:categories) { create_list(:category, 5) }
 
     before(:each) { get url, headers: auth_header(user) }
+    include_examples "forbidden access"
   end
   
   context "POST /categories" do
     let(:url) { "/admin/v1/categories" }
 
     before(:each) { post url, headers: auth_header(user) }
+    include_examples "forbidden access"
   end
   
   context "PATCH /categories/:id" do
@@ -21,6 +23,7 @@ RSpec.describe "Admin V1 Categories as :client", type: :request do
     let(:url) { "/admin/v1/categories/#{category.id}" }
 
     before(:each) { patch url, headers: auth_header(user) }
+    include_examples "forbidden access"
   end
   
   context "DELETE /categories/:id" do
@@ -28,5 +31,6 @@ RSpec.describe "Admin V1 Categories as :client", type: :request do
     let(:url) { "/admin/v1/categories/#{category.id}" } 
 
     before(:each) { delete url, headers: auth_header(user) }
+    include_examples "forbidden access"
   end
 end
