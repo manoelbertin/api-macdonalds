be2abed3e04db0cad0d8e7294a86a7043b977401
manoel bertin <manoeltirj@gmail.com>
2023-05-10 09:34:57 -0300

    Dentro do nosso contexto raiz, já estamos criando uma lista de 15 categorias, que é o model que utilizaremos para testar o nosso service.

--- spec/services/admin/model_loading_service_spec.rb
+++ spec/services/admin/model_loading_service_spec.rb
@@ -1,5 +1,13 @@
 require "rails_helper"
 
 describe Admin::ModelLoadingService do
-  
+  context "when #call" do
+    let!(:categories) { create_list(:category, 15) }
+
+    context "when params are present" do
+    end
+
+    context "when params are not present" do
+    end
+  end  
 end
