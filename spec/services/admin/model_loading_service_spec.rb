b186be2544625ddef9d01abf24f16c6431121cfa
manoel bertin <manoeltirj@gmail.com>
2023-05-12 09:16:00 -0300

    Pegando a   lista search_categories que criamos e ordenando de forma decrescente e extraindo dela os elementos de 4 a 7, que correspondem a página 2 de tamanho 4 que enviamos nos parametros. Depois verificamos se o que foiretornado do servece contém exatamente o que esperamos.

--- spec/services/admin/model_loading_service_spec.rb
+++ spec/services/admin/model_loading_service_spec.rb
@@ -20,6 +20,14 @@ describe Admin::ModelLoadingService do
         result_categories = service.call
         expect(result_categories.count).to eq 4
       end
+
+      it "returns records following search, order and pagination" do
+        search_categories.sort! { |a, b| b[:name] <=> a[:name] }
+        service = described_class.new(Category.all, params)
+        result_categories = service.call
+        expected_categories = search_categories[4..7]
+        expect(result_categories).to contain_exactly *expected_categories
+      end
     end
 
     context "when params are not present" do
