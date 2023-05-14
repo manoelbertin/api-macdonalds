24ec38e2267cce1bc1f0ec9c114bbd87c251de38
manoel bertin <manoeltirj@gmail.com>
2023-05-14 10:12:33 -0300

    .....New Commets were inserted.

--- app/models/concerns/paginatable.rb
+++ app/models/concerns/paginatable.rb
@@ -1,8 +1,8 @@
 module Paginatable
   extend ActiveSupport::Concern
 
-  MAX_PER_PAGE = 10
-  DEFAULT_PAGE = 1
+  MAX_PER_PAGE = 10 # aqui são as 10 categorias q são devolvidas por default lá no contexto... 
+  DEFAULT_PAGE = 1  # ..."When params are not present" em model_loading_service_spec.rb (teste).
 
   included do
     scope :paginate, -> (page, length) do
