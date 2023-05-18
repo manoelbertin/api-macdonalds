bf050e159ebeee323921044e16e0d31eb4613267
manoel bertin <manoeltirj@gmail.com>
2023-05-17 21:01:34 -0300

    Criada a    action show vazia no categories_controller e no before_action add :show.

--- app/controllers/admin/v1/categories_controller.rb
+++ app/controllers/admin/v1/categories_controller.rb
@@ -1,6 +1,6 @@
 module Admin::V1
   class CategoriesController < ApiController
-    before_action :load_category, only: [:update, :destroy]
+    before_action :load_category, only: [:show, :update, :destroy]
     
     def index
       # @categories = Category.all
@@ -13,6 +13,8 @@ module Admin::V1
 
       save_category!
     end
+
+    def show; end
 
     def update
       @category.attributes = category_params
