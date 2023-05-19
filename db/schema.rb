43e4c1f5adb17c9ae3149b9c3fb4f0fd9b326569
manoel bertin <manoeltirj@gmail.com>
2023-05-19 08:37:14 -0300

    Espelhei o  campo adicionado a tabela Products com o db:migrate.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_14_113725) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_19_113256) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -86,6 +86,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_14_113725) do
     t.bigint "productable_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "status"
     t.index ["productable_type", "productable_id"], name: "index_products_on_productable"
   end
 
