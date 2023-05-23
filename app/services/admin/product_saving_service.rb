8b7791ab52275c54598c43d93db49686c4237fae
manoel bertin <manoeltirj@gmail.com>
2023-05-23 20:39:31 -0300

    No construtor estamosseparando, dos parâmetros que recebemos na variável params, os que são de Product e os que serão da entidade asso ciada.Também inicializamos a variável de erros e uma para o produto que está sendo criado ou atualizado.

--- app/services/admin/product_saving_service.rb
+++ app/services/admin/product_saving_service.rb
@@ -3,5 +3,13 @@ module Admin
     class NotSavedProductError < StandardError; end
   
     attr_reader :product, :errors
+
+    def initialize(params, product = nil)
+      params = params.deep_symbolize_keys
+      @product_params = params.reject { |key| key == :productable_attributes }
+      @productable_params = params[:productable_attributes] || {}
+      @errors = {}
+      @product = product || Product.new
+    end
   end
 end
