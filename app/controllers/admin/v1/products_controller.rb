c6923e56c56761440ad54a452cc78534b161afdd
manoel bertin <manoeltirj@gmail.com>
2023-05-25 21:26:39 -0300

    Tanto para oupdate quanto para o create sabemos q precisaremos pegar os parâmetros q chegam no body da requisição. E temos também a preocupação de receber esses parâmetros num mesmo nível e organizá-los.

--- app/controllers/admin/v1/products_controller.rb
+++ app/controllers/admin/v1/products_controller.rb
@@ -21,5 +21,15 @@ module Admin::V1
       permitted = params.permit({ search: :name }, { order: {} }, :page, :length)
       Admin::ModelLoadingService.new(Product.all, permitted).call
     end
+
+    def product_params
+      return {} unless params.has_key?(:product)
+      permitted_params = params.require(:product).permit(:id, :name, :description, :image, 
+                                                         :price, :productable,
+                                                         :status, category_ids: [])
+      permitted_params.merge(productable_params)
+    end
+
+    
   end
 end
