eab86ca089e99b8aae803c5ad6aaa04bccaba0a5
manoel bertin <manoeltirj@gmail.com>
2023-05-25 21:36:29 -0300

    Este método run_service está criando objeto do service Admin::ProductSavingService e passando tanto os product_paramsconvertidos para hash quanto o @product. Este @product ainda não existe, mas para o update nós precisamosdele. E para o caso dele ter uma variável de instância, ele sempre retorna nulo quando ainda não existe. E no final do método atribuímos o novo produto que é um atributo do service em @product e renderizamos a view show.

--- app/controllers/admin/v1/products_controller.rb
+++ app/controllers/admin/v1/products_controller.rb
@@ -22,6 +22,13 @@ module Admin::V1
       Admin::ModelLoadingService.new(Product.all, permitted).call
     end
 
+    def run_service(product = nil)
+      @saving_service = Admin::ProductSavingService.new(product_params.to_h, @product)
+      @saving_service.call
+      @product = @saving_service.product
+      render :show
+    end
+
     def product_params
       return {} unless params.has_key?(:product)
       permitted_params = params.require(:product).permit(:id, :name, :description, :image, 
