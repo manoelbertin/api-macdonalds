64bab83a11882a6011edc57ac7f35f50f403bba7
manoel bertin <manoeltirj@gmail.com>
2023-06-19 09:48:11 -0300

    Updating the user file in the models folder.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,17 @@
 # frozen_string_literal: true
 
 class User < ActiveRecord::Base
-  include NameSearchable
+  include LikeSearchable
   include Paginatable
   
   
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
   include DeviseTokenAuth::Concerns::User
+
+  has_many :wish_items
+  has_many :orders
 
   validates :name, presence: true
   validates :profile, presence: true
